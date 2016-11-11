784a511b8f9d6176a8ac3452a3ed7afd0609dac7
prashant18691 <prashant18691@gmail.com>
2016-11-12 00:25:15 +0530

    Update MyApp.js

--- MyApp.js
+++ MyApp.js
@@ -1,3 +1,5 @@
 var myApp=angular.module('myApp',[]).controller('myController'.function($scope){
 
+                                                $scope.list=[{name:'A',mark:34},{name:'B',mark:67}];
+                                                
 });
