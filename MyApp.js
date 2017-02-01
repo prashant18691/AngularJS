27f992452325f472d479d4063ff8029feec12275
prashant18691 <prashant18691@gmail.com>
2017-02-01 16:56:14 +0530

    Update MyApp.js

--- MyApp.js
+++ MyApp.js
@@ -1,9 +1,22 @@
-var myApp=angular.module('myApp',[]).controller('myController'.function($scope){
-
-     $scope.details=[{name:'A',mark:34},{name:'B',mark:67},{name:'C',mark:70}];
+angular.module('myApp',[]).controller('myController',function($scope){
+	
+     $scope.details=[{name:'B',mark:64},{name:'C',mark:34},{name:'A',mark:10}];
                                                 
      $scope.sortFunc = function(x){
+		 
+	  if(angular.isUndefined($scope.sort)){	
       $scope.sort = x;
+	  return;
+	  }
+	  
+	  if($scope.sort==x)
+		  $scope.sort='-'+x;
+	  else
+		 $scope.sort = x;
+
+		return;
+	  
+	  
   
       }
                                                 
